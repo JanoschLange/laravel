3eecce9a260107702ef4c6a50ce27e0289a8679b
Janosch Lange <janosch.lange@sw-motech.com>
2022-08-12 15:48:16 +0200

    Formatierung

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -4,8 +4,7 @@
 <h1>{{  $heading }}</h1>
 
 @unless (count($listings) == 0)    <!-- außer wenn -->
-@foreach($listings as $listing)
-         
+@foreach($listings as $listing)         
     <h2> 
       <a href="/listings/{{$listing['id']}}"> {{$listing['title']}} </a>
     </h2>
